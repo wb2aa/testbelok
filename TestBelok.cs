52f70438ea9408cbfd35d6bf17e1f598e752eef9
wb2aa <47197729+wb2aa@users.noreply.github.com>
2024-01-14 21:00:30 -0500

    Добавлен тест для генерации изображения додекаэдра DodecahedronX3D

--- TestBelok.cs
+++ TestBelok.cs
@@ -1,5 +1,6 @@
 using Belok.Common.Geometry;
 using Belok.Common.Visualization.BaseGraphics;
+using System.Diagnostics.Metrics;
 using System.Drawing;
 
 namespace TestBelok
@@ -20,10 +21,19 @@ namespace TestBelok
             string[] lines = File.ReadAllLines(pdbPath);
             Dictionary<int, Point_3> points = ExtractHetatmPoints(lines);
             Dictionary<int, List<int>> connections = ExtractConnections(lines);
-            WriteFullerene(x3dPath, points, connections);
+            WriteFrame(x3dPath, points, connections);
             Assert.Pass();
         }
-
+        [Test]
+        public void DodecahedronX3D()
+        {
+            string TestDataPath = "../../../TestData";
+            string x3dPath = Path.Combine(TestDataPath, "dodecahedron.html");
+            Dictionary<int, Point_3> points = Dodecahedron.GetPoints();
+            Dictionary<int, List<int>> connections = Dodecahedron.GetLines();
+            WriteFrame(x3dPath, points, connections);
+            Assert.Pass();
+        }
         //HETATM  449  C1  60C B 101      -0.317 -11.262  -3.737  1.00 28.18           C  
         private Dictionary<int, Point_3> ExtractHetatmPoints(string[] lines)
         {
@@ -67,7 +77,7 @@ namespace TestBelok
             }
             return connections;
         }
-        private void WriteFullerene(string path, Dictionary<int, Point_3> pointspoints, Dictionary<int, List<int>> connections)
+        private void WriteFrame(string path, Dictionary<int, Point_3> pointspoints, Dictionary<int, List<int>> connections)
         {
             double rCylinder = 0.1;
             double viewportZ = 15.0;
@@ -119,6 +129,64 @@ namespace TestBelok
                 center = new Point_3((xMax + xMin)/2.0, (yMax + yMin) / 2.0, (zMax + zMin) / 2.0); 
             }
             return center;
+        }
+
+        public static class Dodecahedron
+        {
+            public static Dictionary<int, Point_3> GetPoints()
+            {
+                Dictionary<int, Point_3> points = new Dictionary<int, Point_3>();
+                double goldenRatio = (1.0 + Math.Sqrt(5.0)) / 2.0;
+                double goldenRatio1 = 1.0 / goldenRatio;
+                int count = 1;
+
+                points.Add(count, new Point_3( 1.0,  1.0,  1.0)); count++;
+                points.Add(count, new Point_3( 1.0,  1.0, -1.0)); count++;
+                points.Add(count, new Point_3( 1.0, -1.0,  1.0)); count++;
+                points.Add(count, new Point_3( 1.0, -1.0, -1.0)); count++;
+
+                points.Add(count, new Point_3(-1.0,  1.0,  1.0)); count++;
+                points.Add(count, new Point_3(-1.0,  1.0, -1.0)); count++;
+                points.Add(count, new Point_3(-1.0, -1.0,  1.0)); count++;
+                points.Add(count, new Point_3(-1.0, -1.0, -1.0)); count++;
+
+                points.Add(count, new Point_3( 0.0,  goldenRatio,  goldenRatio1)); count++;
+                points.Add(count, new Point_3( 0.0,  goldenRatio, -goldenRatio1)); count++;
+                points.Add(count, new Point_3( 0.0, -goldenRatio,  goldenRatio1)); count++;
+                points.Add(count, new Point_3( 0.0, -goldenRatio, -goldenRatio1)); count++;
+
+                points.Add(count, new Point_3( goldenRatio1, 0.0,  goldenRatio)); count++;
+                points.Add(count, new Point_3( goldenRatio1, 0.0, -goldenRatio)); count++;
+                points.Add(count, new Point_3(-goldenRatio1, 0.0,  goldenRatio)); count++;
+                points.Add(count, new Point_3(-goldenRatio1, 0.0, -goldenRatio)); count++;
+
+                points.Add(count, new Point_3( goldenRatio,  goldenRatio1, 0.0)); count++;
+                points.Add(count, new Point_3( goldenRatio, -goldenRatio1, 0.0)); count++;
+                points.Add(count, new Point_3(-goldenRatio,  goldenRatio1, 0.0)); count++;
+                points.Add(count, new Point_3(-goldenRatio, -goldenRatio1, 0.0)); count++;
+
+                return points;
+            }
+            public static Dictionary<int, List<int>> GetLines()
+            {
+                Dictionary<int, List<int>> connections = new Dictionary<int, List<int>>();
+                connections.Add(1, new List<int>() {  9, 13, 17 });
+                connections.Add(2, new List<int>() { 10, 14, 17 });
+                connections.Add(3, new List<int>() { 11, 13, 18 });
+                connections.Add(4, new List<int>() { 12, 18, 14 });
+                connections.Add(5, new List<int>() { 15, 19,  9 });
+                connections.Add(6, new List<int>() { 16, 10, 19 });
+                connections.Add(7, new List<int>() { 20, 11, 15 });
+                connections.Add(8, new List<int>() { 12, 16, 20 });
+
+                connections.Add( 9, new List<int>() { 10 });
+                connections.Add(11, new List<int>() { 12 });
+                connections.Add(13, new List<int>() { 15 });
+                connections.Add(14, new List<int>() { 16 });
+                connections.Add(17, new List<int>() { 18 });
+                connections.Add(19, new List<int>() { 20 });
+                return connections;
+            }
         }
     }
 }
